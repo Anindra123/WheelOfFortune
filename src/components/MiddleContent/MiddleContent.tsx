75cd414c83ecf7bc81bcb91d793d653141f099c6
Anindra123 <bivasdas911@gmail.com>
2024-01-15 00:20:06 +0600

    added required discount and user info ui

--- src/components/MiddleContent/MiddleContent.tsx
+++ src/components/MiddleContent/MiddleContent.tsx
@@ -3,7 +3,58 @@ import "./MiddleContent.css"
 export default function MiddleContent() {
     return (
         <section className="middle-container">
+            <table className="discount-winner-table">
+                <thead>
+                    <tr>
+                        <th className="discount-winner-table-head">Name</th>
+                        <th className="discount-winner-table-head">Email</th>
+                        <th className="discount-winner-table-head">Discount Amount</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr className="discount-winner-table-row">
+                        <td className="discount-winner-table-data">
+                            John Doe
+                        </td>
+                        <td className="discount-winner-table-data">
+                            john@mail.com
+                        </td>
+                        <td className="discount-winner-table-data">
+                            40%
+                        </td>
 
+
+                    </tr>
+                    <tr className="discount-winner-table-row">
+                        <td className="discount-winner-table-data">
+                            John Doe
+                        </td>
+                        <td className="discount-winner-table-data">
+                            john@mail.com
+                        </td>
+                        <td className="discount-winner-table-data">
+                            40%
+                        </td>
+
+
+                    </tr>
+                    <tr className="discount-winner-table-row">
+                        <td className="discount-winner-table-data">
+                            John Doe
+                        </td>
+                        <td className="discount-winner-table-data">
+                            john@mail.com
+                        </td>
+                        <td className="discount-winner-table-data">
+                            40%
+                        </td>
+
+
+                    </tr>
+
+
+                </tbody>
+            </table>
         </section>
     )
 }
--- src/components/SpinnerContentList/SpinnerContentList.tsx
+++ src/components/SpinnerContentList/SpinnerContentList.tsx
@@ -5,17 +5,18 @@ interface SpinnerContentListProps {
 }
 
 export default function SpinnerContentList({ spinnerContent }: SpinnerContentListProps) {
-
     return (
         <table>
             <thead>
-                <th>Discount</th>
-                <th>Type</th>
-                <th>Color</th>
+                <tr>
+                    <th>Discount</th>
+                    <th>Type</th>
+                    <th>Color</th>
+                </tr>
             </thead>
             <tbody>
                 {[...spinnerContent.entries()].map((entry) => (
-                    <tr>
+                    <tr key={entry[0]}>
                         <td>{entry[1].discountAmount}</td>
                         <td>{entry[1].discountType}</td>
                         <td style={{ display: "flex", justifyContent: "center" }}><div style={{ width: "2rem", alignSelf: "center", height: "2rem", backgroundColor: entry[1].discountColor }}></div></td>
